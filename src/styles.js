fa0724e06b9d7973939eb04ca49007c36ab0d63f
Daria Łuczak <daria.luczak@monterail.com>
2018-03-19 19:55:53 +0100

    style submit button

--- src/styles.js
+++ src/styles.js
@@ -20,20 +20,8 @@ injectGlobal`
     h1 {
       margin: 0;
     }
-
-    button {
-      cursor: pointer;
-      padding: 12px;
-      background: #999;
-      font-family: 'Roboto Slab', sans-serif;
-      font-size: 16px;
-      border: none;
-      outline: none;
-      color: white;
-      border-bottom: 2px solid #797979;
-    }
     
     .displayErrors input:invalid, .displayErrors textarea:invalid, .displayErrors select:invalid {
-      border-color: red;
+      border-color: rgb(244, 181, 180);
     }
 `;
